62701edc0e8848569f216e71e02da3aef2bba14d
Bogdan <->
2020-10-09 22:21:56 +0300

    Lucrul Individual

--- Lucrul Individual/index.js
+++ Lucrul Individual/index.js
@@ -1,27 +1,97 @@
 var app = angular.module("app", []);
-app.controller("appCtrl", function($scope) {
+
+// directiva proprie
+app.directive("nrOwnTickets", function() {
+  return {
+    template : "<h5>Numarul de bilete procurate = {{registeredUsers[loggedUserId].tickets.length}}</h5>"
+  }
+});
+
+// serviciu propriu
+app.service('sum', function() {
+  this.nrMyTickets = function (x) {
+    return x.length;
+  }
+});
+
+app.controller("appCtrl", function($scope, $interval, sum) {
   $scope.movies = [
-    {name: "Tenet", year: 2020, month: 07, genre: "Actiune", image: "./img/Tenet.jpg"},
-    {name: "Soul", year: 2020,  month: 11, genre: "Desen animat", image: "./img/Soul.jpg"},
-    {name: "Bad Boys For Life", year: 2020,  month: 01, genre: "Actiune", image: "./img/Bad_Boys_For_Life.jpg"},
-    {name: "Palm Springs", year: 2020, month: 07, genre: "Comedie", image: "./img/Palm_Springs.jpg"},
-    {name: "Greyhound", year: 2020, month: 07, genre: "Drama", image: "./img/Greyhound.jpg"},
-    {name: "Sonic the Hedgehog", year: 2020, month: 02, genre: "Comedie", image: "./img/Sonic_the_Hedgehog.jpg"},
-    {name: "Black Widow", year: 2020, month: 10, genre: "Actiune", image: "./img/Black_Widow.jpg"},
-    {name: "Onward", year: 2020, month: 02, genre: "Desen animat", image: "./img/Onward.jpg"},
-    // {name: "Wonder Woman 1984", year: "2020.06", genre: "Actiune", image: "./img/Wonder_Woman_1984.jpg"},
-    // {name: "", year: "", genre: "", image: "./img/.jpg"},
+    {
+      name: "Tenet",
+      year: 2020,
+      month: 07,
+      genre: "Actiune",
+      image: "./img/Tenet.jpg",
+      show:["17/10/2020 15:00", "21/10/2020 17:00"],
+    },
+    {
+      name: "Soul",
+      year: 2020,
+      month: 11,
+      genre: "Desen animat",
+      image: "./img/Soul.jpg",
+      show:["28/11/2020 16:00", "29/11/2020 18:00"],
+    },
+    {
+      name: "Bad Boys For Life",
+      year: 2020,
+      month: 01,
+      genre: "Actiune",
+      image: "./img/Bad_Boys_For_Life.jpg",
+      show:["16/10/2020 12:00", "18/10/2020 18:00"],
+    },
+    {
+      name: "Palm Springs",
+      year: 2020,
+      month: 07,
+      genre: "Comedie",
+      image: "./img/Palm_Springs.jpg",
+      show:["19/10/2020 11:00", "25/10/2020 14:00"],
+    },
+    {
+      name: "Greyhound",
+      year: 2020,
+      month: 07,
+      genre: "Drama",
+      image: "./img/Greyhound.jpg",
+      show:["18/10/2020 14:00", "15/10/2020 19:00"],
+    },
+    {
+      name: "Sonic the Hedgehog",
+      year: 2020,
+      month: 02,
+      genre: "Comedie",
+      image: "./img/Sonic_the_Hedgehog.jpg",
+      show:["24/10/2020 18:00", "28/10/2020 20:00"],
+    },
+    {
+      name: "Black Widow",
+      year: 2020,
+      month: 10,
+      genre: "Actiune",
+      image: "./img/Black_Widow.jpg",
+      show:["17/10/2020 16:00", "17/10/2020 19:00"],
+    },
+    {
+      name: "Onward",
+      year: 2020,
+      month: 02,
+      genre: "Desen animat",
+      image: "./img/Onward.jpg",
+      show:["22/10/2020 10:00", "23/10/2020 14:00"],
+    },
   ];
 
   $scope.isLoggedIn = false;
   $scope.userInput = {};
-  $scope.registeredUser = [
+  $scope.registeredUsers = [
   {
     "userId": 0,
-    "email": "2@2.com",
-    "password": "2@22@2"
+    "email": "2@2",
+    "password": "2@22@2",
+    "tickets": ["Tenet", "Soul"],
   }];
-  nrOfUsers = $scope.registeredUser.length;
+  nrOfUsers = $scope.registeredUsers.length;
 
   $scope.toPage = function(page) {
     $scope.userInput = {};
@@ -30,10 +100,11 @@ app.controller("appCtrl", function($scope) {
   };
   
   $scope.addUser = function(user) {
-    $scope.registeredUser.push({
+    $scope.registeredUsers.push({
       "userId": nrOfUsers++,
       "email": user.email,
-      "password": user.password
+      "password": user.password,
+      "tickets": [],
     });
     $scope.userInput = {};
     $scope.userIsAdded = true;
@@ -41,10 +112,11 @@ app.controller("appCtrl", function($scope) {
 
   $scope.verifyExistingUser = function(user) {
     for (i = 0; i < nrOfUsers; i++) {
-      if (user.email == $scope.registeredUser[i].email && user.password == $scope.registeredUser[i].password) {
+      if (user.email == $scope.registeredUsers[i].email && user.password == $scope.registeredUsers[i].password) {
         $scope.isLoggedIn = true;
         $scope.loginError = false;
         $scope.userInput = {};
+        $scope.loggedUserId = $scope.registeredUsers[i].userId;
         $scope.activePage= "home.html";
         break;
       } else {
@@ -55,6 +127,16 @@ app.controller("appCtrl", function($scope) {
 
   $scope.logout = function() {
     $scope.isLoggedIn = false;
+    $scope.activePage= "home.html";
   }
   
+  $scope.addTicket = function(ticket) {
+    $scope.registeredUsers[$scope.loggedUserId].tickets.push(ticket);
+    $scope.activePage= "myTickets.html";
+  }
+
+  currentTime = new Date().toLocaleTimeString('uk-UK');
+  $interval(function () {
+    $scope.currentTime = new Date().toLocaleTimeString('uk-UK');
+  }, 1000);
 });
